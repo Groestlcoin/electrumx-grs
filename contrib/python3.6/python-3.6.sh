c0d4bf44775bcdac55c5070d4eb229f2e4158cd2
Gruve_p <gruve-p@users.noreply.github.com>
2017-06-06 12:44:48 +0200

    Update python-3.6.sh

--- contrib/python3.6/python-3.6.sh
+++ contrib/python3.6/python-3.6.sh
@@ -7,7 +7,7 @@ sudo add-apt-repository ppa:jonathonf/python-3.6
 sudo apt-get update && sudo apt-get install python3.6 python3.6-dev
 
 cd /home/username
-git clone https://github.com/kyuupichan/electrumx.git
-cd electrumx
+git clone https://github.com/groestlcoin/electrumx-grs.git
+cd electrumx-grs
 sudo python3.6 setup.py install
 
