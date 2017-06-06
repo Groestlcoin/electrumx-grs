be5be9d9c13767e2c06a0a3d3669c7fada273985
Gruve_p <gruve-p@users.noreply.github.com>
2017-06-06 12:04:24 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,21 +3,21 @@ from server.version import VERSION
 
 
 setuptools.setup(
-    name='electrumx',
+    name='electrumx-grs',
     version=VERSION.split()[-1],
     scripts=['electrumx_server.py', 'electrumx_rpc.py'],
     python_requires='>=3.5.3',
     # "irc" package is only required if IRC connectivity is enabled
     # via environment variables, in which case I've tested with 15.0.4
-    # "x11_hash" package (1.4) is required to sync DASH network.
+    # "groestlcoin_hash" package is required to sync GRS network.
     install_requires=['plyvel', 'pylru', 'irc', 'aiohttp >= 1', 'groestlcoin_hash'],
     dependency_links=['git+https://github.com/groestlcoin/groestlcoin-hash-python#egg=groestlcoin_hash'],
     packages=setuptools.find_packages(),
-    description='ElectrumX Server',
-    author='Neil Booth',
-    author_email='kyuupichan@gmail.com',
+    description='ElectrumX-GRS Server',
+    author='Kefkius',
+    author_email='kefkius@gmail.com',
     license='MIT Licence',
-    url='https://github.com/kyuupichan/electrumx/',
+    url='https://github.com/Groestlcoin/electrumx-grs',
     long_description='Server implementation for the Electrum wallet',
     classifiers=[
         'Development Status :: 3 - Alpha',
